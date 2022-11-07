5bbdb2a5944bffa1795bfc5433bee4028100a238
linhSon257 <sonnlgch18875@fpt.edu.vn>
2022-11-07 23:39:21 +0700

    update1

--- src/index.js
+++ src/index.js
@@ -1,21 +1,29 @@
 const express = require ('express')
+const sass = require('node-sass');
 const path = require ('path')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars')
 const app = express()
-const port = 3001
+const port = 3001;
 
+
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('combined'))
 
 //template engine
-app.engine('handlebars', engine())
-app.set('view engine', 'handlebars')
+app.engine('hbs', engine({
+    extname: '.hbs'
+}))
+app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-//console.log('PATH: ', path.join(__dirname, 'resources/views') )
 
 app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.listen (port, () => console.log('Example app listening at http://localhostis:${port}'))
+app.get('/news', (req, res) => {
+    res.render('news')
+})
+
+app.listen (port, () => console.log('Example app listening at ${port}'))
